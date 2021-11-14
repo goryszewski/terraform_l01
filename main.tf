548856056ad90044afc75a0eecd264ba2d70e6cf
Michal Goryszewski <goryszewski@gmail.com>
2021-11-14 13:12:24 +0100

    add [jenkins,rsyslog,jenkins-node]

--- main.tf
+++ main.tf
@@ -1,5 +1,27 @@
-module "node201"{
+module "pl1-node201-api"{
   source ="./libvirt"
-  hostname ="node201"
+  hostname ="pl1-node201"
+  domain = "api.cloud.local"
   ip="201"
+}
+
+module "pl1-rsyslog01-core"{
+  source ="./libvirt"
+  domain = "core.cloud.local"
+  hostname ="pl1-rsyslog01"
+  ip="101"
+}
+
+module "pl1-jenkins01-core"{
+  source ="./libvirt"
+  domain = "core.cloud.local"
+  hostname ="pl1-jenkins01"
+  ip="102"
+}
+
+module "pl1-jenkins01-node01-core"{
+  source ="./libvirt"
+  domain = "core.cloud.local"
+  hostname ="pl1-jenkins01-node01"
+  ip="103"
 }
